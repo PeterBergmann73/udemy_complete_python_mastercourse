7af804cca0a48f2a6c99d2ef0c9385650e78e985
PeterBergmann73 <pbergmann73@yahoo.com>
2017-12-11 11:02:08 +0100

    Took the solution from the class.

--- .idea/python/challenge.py
+++ .idea/python/challenge.py
@@ -1,29 +1,17 @@
 ipAddress = input("Please enter the IP address: ")
-nmbSegments = 0
+segmentNmb = 1
 segmentLength = 0
 dot = "."
 
+if ipAddress[-1] != dot:
+    ipAddress += dot
+
+# we define a segment as somethin that ends with a "."
 # we increase segment count at the end of the segment
-for i in range(0, len(ipAddress)):
-    char = ipAddress[i]
-    if i == len(ipAddress) - 1:
-        if char != dot:
-            segmentLength += 1
-            nmbSegments += 1
-        else:
-            if segmentLength > 0:
-                nmbSegments += 1
-
-        if nmbSegments > 0:
-            print("Segment number: {0}, segment length: {1}".format(nmbSegments, segmentLength))
+for char in ipAddress:
+    if char == dot:
+        print("segment {} contains {} charachters".format(segmentNmb, segmentLength))
+        segmentNmb += 1
+        segmentLength = 0
     else:
-        if char != dot:
-            segmentLength += 1
-        else:
-            if segmentLength > 0:  # closing dot
-                nmbSegments += 1
-                # restart segment length
-                print("Segment number: {0}, segment length: {1}".format(nmbSegments, segmentLength))
-                segmentLength = 0
-
-print("Total number of segments: {0}".format(nmbSegments))
+        segmentLength += 1
