b1ea892020e109ccfe4986cfacb509eebc6c1d21
PeterBergmann73 <pbergmann73@yahoo.com>
2017-12-11 18:37:15 +0100

    Added tuples.

---
+++ .idea/python/tuples.py
@@ -0,0 +1,22 @@
+# t = "a", "b", "c"
+# print(t)
+#
+# print("a", "b", "c")
+# print(("a", "b", "c"))
+
+imelda = "More Mayhem", "Emilda May", 2011
+metallica  ="Ride the Lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+# metallica[0] = "Master of Puppets"
+imelda = imelda[0], "Imelda May", imelda[2]
+print(imelda)
+
+metallica2 = ["Ride the Lightning", "Metallica", 1984]
+print(metallica2)
+metallica2[0] = "Master of Puppets"
+print(metallica2)
